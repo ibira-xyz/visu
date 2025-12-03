203cc867ed153ef7217e46f7476e4db5a9b2d220
Jonas Rocha Lima Amaro <jonasrla@gmail.com>
2025-12-03 00:31:43 -0300

    implementa o backend da AWS

--- src/backend/aws_backend.py
+++ src/backend/aws_backend.py
@@ -1,18 +1,41 @@
+""""AWS Backend Module."""
+import boto3
+from boto3.dynamodb.conditions import Key
+
 from backend.backend import Backend
+from config.app_config import AppConfig
+
+config = AppConfig()
 
 class AwsBackend(Backend):
     """A reader that reads content from AWS S3 and DynamoDB."""
     def __init__(self, remote_url):
         self.remote_url = remote_url
+        self.dynamodb = boto3.resource('dynamodb')
+        self.s3 = boto3.client('s3')
+
+        self.bucket_name = config.get('assets_bucket')
 
     def get_post(self, slug):
-        # Implementation to fetch post from remote URL
-        pass
+        table = self.dynamodb.Table('PostContent')
+        response = table.query(KeyConditionExpression=Key('PostID').eq(slug))
+        item = response.get('Items')
+        if item:
+            return {'slug': slug, **item[0]['Content']}
+        else:
+            return None
 
-    def get_content(self, content_name):
-        # Implementation to fetch content from remote URL
-        pass
+    def get_content(self, content_uri):
+        bucket = self.bucket_name
+        key = content_uri.lstrip('/')
+        response = self.s3.get_object(Bucket=bucket, Key=key)
+        content = response['Body'].read()
+        content = content.decode('utf-8')
+        return content
 
     def fetch_index_data(self):
-        # Implementation to fetch index data from remote URL
-        pass
+        table = self.dynamodb.Table('PostContent')
+        response = table.scan()
+        items = response.get('Items', [])
+        for item in items:
+            yield {'slug': item['PostID'], **item['Content']}
